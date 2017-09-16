7d0d38158037e74bc4123d52637a1644f5de7826
uasalian <umesh_salian@hotmail.com>
2017-09-16 16:23:22 -0500

    Initial version

---
+++ src/com/example/test/CSRFScanner.java
@@ -0,0 +1,103 @@
+package com.example.test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import net.sourceforge.jwebunit.junit.WebTester;
+
+public class CSRFScanner {
+	
+	private WebTester tester;
+	
+	private String baseURL, scanURL, logoutURL;
+	private String csrfTokenId, csrfErrorResponse;
+	
+	public void log(String s) {System.out.println(s); }
+
+    @Before
+    public void prepare() {
+    	baseURL = "http://www.example.com";
+    	scanURL = baseURL + "/pageWithForm/";
+    	logoutURL = baseURL + "/logout";
+    	
+    	// id of hidden field carrying anti-csrf token
+    	csrfTokenId = "csrfToken";
+    	// this unique string is included in error response on csrf token validation failure
+    	csrfErrorResponse = "CSRFTokenValidationError";
+    	
+    	tester = new WebTester();
+    }
+    
+    public void login() {
+    	//get the login page
+        tester.beginAt(baseURL);
+        // Set the username
+        tester.setTextField("userName", "<username>");
+        // set the password
+        tester.setTextField("passWord", "<password>");
+        // Submit login. The button name here is "login"
+        tester.submit("login");
+        // On login, get the page of interest to scan for csrf vulnerability
+        tester.gotoPage(scanURL);
+    }
+    
+    public void logout() {
+    	tester.gotoPage(logoutURL);
+    }
+
+    @Test
+    public void scan() {
+    	testTokenExists();
+    	testTokenHasValue();
+    	testValidToken();
+    	testInvalidToken();
+    }
+    
+    /**
+     * Check if csrf token exists in the form
+     */
+	 private void testTokenExists() {	  
+		  login();
+		  boolean hasCsrfToken = tester.hasElementById(csrfTokenId);
+		  logout();
+		  Assert.assertTrue("No Anti-CSRF token in the form", hasCsrfToken);
+	 }
+
+	 /** 
+	  * Check if the token is populated
+	  */
+	 private void testTokenHasValue() {	  
+		  login();
+		  String tokenVal = tester.getElementById(csrfTokenId).getAttribute("value");
+		  logout();
+		  Assert.assertFalse("Anti-CSRF token has no value", tokenVal.isEmpty());
+	 }
+	 
+	 /** 
+	  * Submit the page without making any changes.
+	  * Whether there is server side validation or not, we do not know what response to expect at this point.
+	  * All we know is that we should NOT get the error response corresponding to CSRF token mismatch.
+	  */
+	 private void testValidToken() {
+		  login();
+		  tester.submit();
+		  boolean hasErrorResponse = tester.getPageSource().contains(csrfErrorResponse);
+		  logout();
+		  Assert.assertFalse("Anti-CSRF Token Mismatch Error response on valid token", hasErrorResponse);
+	 }
+	 
+	 /**
+	  * In this test we modify the CSRF token and submit the page
+	  * We must get the unique string specifically configured to indicate the server side validation error.
+	  * If not, that indicates there is no server side validation of CSRF token.
+	  */
+	 private void testInvalidToken() {
+		  login();
+		  tester.setHiddenField(csrfTokenId, "XXX");
+		  tester.submit();
+		  boolean hasErrorResponse = tester.getPageSource().contains(csrfErrorResponse);
+		  logout();
+		  Assert.assertTrue("No Anti-CSRF Token Mismatch Error response on Invalid token", hasErrorResponse);		  
+	 }
+}
---
+++ src/com/example/test/TestSuite.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+	@RunWith(Suite.class)
+	@Suite.SuiteClasses({
+		CSRFScanner.class
+	})
+
+public class TestSuite {
+
+}
